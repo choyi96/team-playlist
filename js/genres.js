b932f9dd054e815184b71c4d5fdd21e1f6b2875b
choyi <choyi96713@gmail.com>
2026-07-01 11:08:22 +0900

    장르별 음악 추천 페이지 완성

--- js/genres.js
+++ js/genres.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.genre-card');
- 
-    cards.forEach(card => {
-      card.addEventListener('click', () => {
-        const isOpen = card.classList.contains('open');
+window.onload = function() {
+  const cards = document.querySelectorAll('.genre-card');
 
-        if (!isOpen) {
-          card.classList.add('open');
-        }
-      });
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function() {
+      if (this.classList.contains('open') == false) {
+        this.classList.add('open');
+      }else {
+        this.classList.remove('open');
+      }
     });
-});
+  }
+};
