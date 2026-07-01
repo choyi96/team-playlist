1851c6ef422ed1881916cdd8dfbafe6ead4398c7
apple12345841-blip <apple12345841@gmail.com>
2026-07-02 08:05:35 +0900

    7월 2일 오전

--- js/artist.js
+++ js/artist.js
@@ -10,6 +10,24 @@ const artistData = {
         debut: "2008년 9월 18일",
         bio: "대한민국의 싱어송라이터이자 배우로, 독보적인 음원 파워와 깊은 감성을 가진 아티스트입니다.",
         tracks: ["좋은 날", "밤편지", "에잇 (prod. & feat. SUGA)", "Love wins all"]
+    },
+    blackpink: {
+        name: "블랙핑크 (BLACKPINK)",
+        debut: "2016년 8월 8일",
+        bio: "대한민국의 4인조 걸그룹으로, 강렬한 음악과 스타일리시한 매력으로 전 세계 음악 시장과 패션계를 사로잡은 글로벌 탑 아티스트입니다.",
+        tracks: ["DDU-DU DDU-DU", "Kill This Love", "How You Like That", "Pink Venom"]
+    },
+    newjeans: {
+        name: "뉴진스 (NewJeans)",
+        debut: "2022년 7월 22일",
+        bio: "대한민국의 5인조 걸그룹으로, 독창적인 콘셉트와 신선한 Y2K 감성의 음악을 선보이며 데뷔와 동시에 글로벌 신드롬을 일으킨 팀입니다.",
+        tracks: ["Hype Boy", "Attention", "Ditto", "OMG"]
+    },
+    seventeen: {
+        name: "세븐틴 (SEVENTEEN)",
+        debut: "2015년 5월 26일",
+        bio: "대한민국의 13인조 보이그룹으로, 작사·작곡·안무 제작을 직접 해내는 '자체 제작 아티스트'이자 압도적인 퍼포먼스를 자랑하는 글로벌 대세입니다.",
+        tracks: ["아주 NICE", "손오공", "울고 싶지 않아", "God of Music"]
     }
 };
 
