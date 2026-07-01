530ff02db5c22461c85710d0eab6f9cea0289ed7
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-07-01 12:23:00 +0900

    lyrics complete

---
+++ playlist/lyrics_data.js
@@ -0,0 +1,463 @@
+export const playlist = [
+    {title: "mosi mosi?", artist: "楽音", youtube: "https://www.youtube.com/embed/jcKUEQMCqYc", lyricText: `ないしらせは 良いしらせ<br>
+南極しらせは お幸せ<br>
+にっちもさっちもいかない<br>
+ニッチな気持ち とっ散らかってるの<br>
+<br>
+無我夢中 小宇宙<br>
+交信中 君にCHU<br>
+彷徨う 貴方様よ<br>
+<br>
+もしもーし、聞こえてる？<br>
+<br>
+電波 電波の怪電波<br>
+宇宙に夢中で交信中<br>
+けんぱけんぱで1000メーター<br>
+ネコに嫌われても ママに叱られても<br>
+わたしと似合う あなたに出会うの<br>
+<br>
+CQCQ どこなのエイリアン<br>
+シクシク 孤独なエリア<br>
+ウォーアイニー ハローハワユー？<br>
+ラブアンドピース 波長が合う<br>
+<br>
+うちからそこまで何光年？<br>
+てかそれとも隣の3丁目？<br>
+キャトったハートは いずこ？<br>
+***QLOダウンじゃ 宇宙は is it cold？<br>
+<br>
+なのにさ姿が 見当たんない<br>
+グレーな気持ちが 宙ぶらりん<br>
+見つけてやるぞ UFO！<br>
+そんで会いたかったって 言うの<br>
+<br>
+電波 電波 …あっ圏外<br>
+それでも 夢中で交信中<br>
+なんか変だよインベーダー<br>
+きみだけに注ぐ愛の<br>
+<br>
+電波 電波の 怪電波<br>
+宇宙に夢中で交信中<br>
+なんか変だよインベーダー<br>
+きみだけに捧ぐ愛を Ah<br>
+<br>
+電波 電波 赤い電波<br>
+リボンに包んで 送信中<br>
+電波 電波 聞こえてるの？<br>
+<br>
+心さらわれても<br>
+いまわたしの<br>
+わたしだけの<br>
+あなたに出会うの<br>
+<br>
+4season<br>
+I miss you<br>
+愛密集<br>`},
+    {title: "Good Luck, Babe!", artist: "Chappell Roan", youtube: "https://www.youtube.com/embed/VZ-oGLluGAc?si=0Ic3jQJhhHcGw7q8", lyricText: `It's fine, it's cool<br>
+You can say that we are nothing, but you know the truth<br>
+And guess I'm the fool<br>
+With her arms out like an angel through the car sunroof<br>
+<br>
+I don't wanna call it off<br>
+But you don't wanna call it love<br>
+You only wanna be the one that I call "baby"<br>
+<br>
+You can kiss a hundred boys in bars<br>
+Shoot another shot, try to stop the feeling<br>
+You can say it's just the way you are<br>
+Make a new excuse, 'nother stupid reason<br>
+Good luck, babe (Well, good luck)<br>
+Well, good luck, babe (Well, good luck)<br>
+You'd have to stop the world just to stop the feeling<br>
+Good luck, babe (Well, good luck)<br>
+Well, good luck, babe (Well, good luck)<br>
+You'd have to stop the world just to stop the feeling<br>
+<br>
+I'm cliché, who cares?<br>
+It's a sexually explicit kind of love affair<br>
+And I cry, it's not fair<br>
+I just need a little lovin', I just need a little air<br>
+<br>
+Think I'm gonna call it off<br>
+Even if you call it love<br>
+I just wanna love someone who calls me "baby"<br>
+<br>
+You can kiss a hundred boys in bars<br>
+Shoot another shot, try to stop the feeling<br>
+You can say it's just the way you are<br>
+Make a new excuse, 'nother stupid reason<br>
+Good luck, babe (Well, good luck)<br>
+Well, good luck, babe (Well, good luck)<br>
+You'd have to stop the world just to stop the feeling<br>
+Good luck, babe (Well, good luck)<br>
+Well, good luck, babe (Well, good luck)<br>
+You'd have to stop the world just to stop the feeling<br>
+<br>
+Ah, ah-yeah, oh<br>
+When you wake up next to him in the middle of the night<br>
+With your head in your hands, you're nothing more than his wife<br>
+And when you think about me all of those years ago<br>
+You're standing face to face with "I told you so"<br>
+You know I hate to say, I told you so<br>
+You know I hate to say, but I told you so<br>
+<br>
+You can kiss a hundred boys in bars<br>
+Shoot another shot, try to stop the feeling (Well, I told you so)<br>
+You can say it's just the way you are<br>
+Make a new excuse, 'nother stupid reason<br>
+Good luck, babe (Well, good luck)<br>
+Well, good luck, babe (Well, good luck)<br>
+You'd have to stop the world just to stop the feeling<br>
+Good luck, babe (Well, good luck)<br>
+Well, good luck, babe (Well, good luck)<br>
+You'd have to stop the world just to stop the feeling<br>
+<br>
+You'd have to stop the world just to stop the feeling<br>
+You'd have to stop the world just to stop the feeling<br>
+You'd have to stop the world just to stop the feeling<br>`},
+    {title: "One Of The Girls", artist: "The Weeknd", youtube: "https://www.youtube.com/embed/f1r0XZLNlGQ?si=Uhu-eVTO51r8jbCO", lyricText: 
+`Lock me up and throw away the key<br>
+He knows how to get the best out of me<br>
+I'm no force for the world to see<br>
+Trade my whole life just to be<br>
+<br>
+Tell nobody I control you<br>
+I broke you just to own you<br>
+They can't tell but I love you<br>
+'Cause you're loyal, baby<br>
+I love when you're submissive<br>
+You love it when I break skin<br>
+You feel pain without flinchin'<br>
+So say it<br>
+<br>
+Give me tough love<br>
+Leave me with nothin' when I come down<br>
+My kind of love<br>
+Force me and choke me 'til I pass out<br>
+<br>
+We don't gotta bе in love, no<br>
+I don't gotta be the onе, no<br>
+I just wanna be one of your girls tonight<br>
+We don't gotta be in love, no<br>
+I don't gotta be the one, no<br>
+I just wanna be one of your girls tonight, oh<br>
+<br>
+Push me down, hold me down<br>
+Spit in my mouth while you turn me out<br>
+I wanna take your light inside<br>
+Tear me down, snuff me out<br>
+Hands on my neck while you push it up<br>
+And I'm screamin' now<br>
+<br>
+Give me tough love<br>
+Leave me with nothin' when I come down<br>
+My kind of love<br>
+Force me and choke me 'til I pass out<br>
+<br>
+We don't gotta be in love, no<br>
+I don't gotta be the one, no<br>
+I just wanna be one of your girls tonight (Tonight)<br>
+We don't gotta be in love, no<br>
+I don't gotta be the one, no (Oh)<br>
+I just wanna be one of your girls tonight, oh<br>
+<br>
+Lock me up and throw away the key<br>
+He knows how to get the best out of me<br>
+I'm no force for the world to see<br>
+Trade my whole life just to be<br>
+Top of the world, but I'm still not free<br>
+This is a secret that I keep<br>
+Until it's gone, I can never find peace<br>
+Waste my whole life just to be<br>
+<br>
+We don't gotta be in love, no<br>
+I don't gotta be the one, no<br>
+I just wanna be one of your girls tonight (Tonight)<br>
+We don't gotta be in love, no<br>
+I don't gotta be the one, no (Oh)<br>
+I just wanna be one of your girls tonight (Oh), oh`},
+    {title: "Like Him", artist: "Tyler, The Creator", youtube: "https://www.youtube.com/embed/dgUHE8wWhiE?si=1IQa0nECLnEwYaXw", lyricText: `Damn, nigga, every time I look at you, I swear to God<br>
+Nigga, you got that nigga feet<br>
+You got that nigga body<br>
+You got that nigga long arms, fingers and shit<br>
+Flat feet, big di—<br>
+
+<br>
+She said that I make expressions like him<br>
+My legs to my shoulders and my chin like him<br>
+My waist and my posture like him<br>
+Like him<br>
+Like him, like him, like him<br>
+<br>
+Mama, I'm chasin' a ghost<br>
+I don't know who he is<br>
+Mama, I'm chasin' a ghost<br>
+I don't know where he is<br>
+Mama, I'm chasin' a ghost<br>
+Do I look like him? (Like what?)<br>
+Like him (Like what?)<br>
+Like him (Like what?)<br>
+Like him (La-la-la-la)<br>
+<br>
+(La-la-la-la)<br>
+<br>
+You gave me love and affection<br>
+Attention (Go), protection<br>
+How could I ever miss somethin' (Go)<br>
+That I'd never had?<br>
+I would nevеr judge ya<br>
+'Cause evеrything worked out without him (Like what?)<br>
+Like him (Like what?)<br>
+Like him (Huh?)<br>
+<br>
+Mama, I'm chasin' a ghost<br>
+I don't know who he is<br>
+Mama, I'm chasin' a ghost<br>
+I don't know where he is<br>
+Mama, I'm chasin' a ghost<br>
+Do I look<br>
+(Huh?)<br>
+<br>
+Like him<br>
+Like him<br>
+<br>
+I decided to<br>
+Anything that lives inside of you<br>
+I would never ever lie to you (Yeah)<br>
+You ain't ever gotta lie to me<br>
+I'm everything that I've strived to be<br>
+So do I look like him?<br>
+Do I look like him? (Like him, like him, like him, like him)<br>
+I don't look like him<br>
+(Like him)<br>
+<br>
+It was my fault, not yours, not his, it was my fault, I'm sorry<br>
+You know<br>
+It was my fault<br>
+Not him, 'cause he always wanted to be there for you<br>
+And I'm sorry I was young<br>
+But he's always wanted to be a father to you<br>
+So I, I fucked up and I take ownership of that<br>
+Of my choices and decisions<br>
+And I'm sorry for that<br>
+He's a good guy<br>
+So don't hold that against him, because it was my fault<br>
+Just, you know, forgive me<br>`},
+    {title: "Money Trees", artist: "Kendrick Lamar", youtube: "https://www.youtube.com/embed/Iy-dJwHVX84?si=lo1iTP0U3pKGY6_m", lyricText: `Uh, me and my niggas tryna get it, ya bish (Ya bish, ya bish)<br>
+Hit the house lick, tell me, is you with it, ya bish? (Ya bish, ya bish)<br>
+Home invasion was persuasive (Was persuasive, was persuasive)<br>
+From nine to five, I know it's vacant, ya bish (Ya bish, ya bish)<br>
+Dreams of livin' life like rappers do (Like rappers do, like rappers do)<br>
+Back when condom wrappers wasn't cool (They wasn't cool, they wasn't cool)<br>
+I fucked Sherane and went to tell my bros (Tell my bros, tell my bros)<br>
+Then Usher Raymond, "Let It Burn" came on ("Let Burn" came on, "Let Burn" came on)<br>
+Hot sauce all in our Top Ramen, ya bish (Ya bish, ya bish)<br>
+Park the car, then we start rhymin', ya bish (Ya bish, ya bish)<br>
+The only thing we had to free our mind (Free our mind, free our mind)<br>
+Then freeze that verse when we see dollar signs (See dollar signs, see dollar signs)<br>
+You lookin' like an easy come-up, ya bish (Ya bish, ya bish)<br>
+A silver spoon, I know you come from, ya bish (Ya bish, ya bish)<br>
+And that's a lifestyle that we never knew (We never knew, we never knew)<br>
+Go at a reverend for the revenue<br>
+<br>
+It go Halle Berry or hallelujah<br>
+Pick your poison, tell me what you doin'<br>
+Everybody gon' respect the shooter<br>
+But the one in front of the gun lives forever<br>
+(The one in front of the gun, forever)<br>
+And I been hustlin' all day, this-a-way, that-a-way<br>
+Through canals and alleyways, just to say<br>
+Money trees is the perfect place for shade<br>
+And that's just how I feel, nah, nah<br>
+A dollar might just fuck your main bitch<br>
+That's just how I feel, nah<br>
+A dollar might say fuck them niggas that you came with<br>
+That's just how I feel, nah, nah<br>
+A dollar might just make that lane switch<br>
+That's just how I feel, nah<br>
+A dollar might turn to a million and we all rich<br>
+That's just how I feel<br>
+<br>
+Dreams of livin' life like rappers do (Like rappers do, like rappers do)<br>
+Bump that new E-40 after school (Way after school, way after school)<br>
+You know, "Big Ballin' With My Homies" (My homies)<br>
+Earl Stevens had us thinkin' rational (Thinkin' rational, that's rational)<br>
+Back to reality, we poor, ya bish (Ya bish, ya bish)<br>
+Another casualty at war, ya bish (Ya bish, ya bish)<br>
+Two bullets in my Uncle Tony head (My Tony head, my Tony head)<br>
+He said one day I'll be on tour, ya bish (Ya bish, ya bish)<br>
+That Louis Burgers never be the same (Won't be the same, won't be the same)<br>
+A Louis belt'll never ease that pain (Won't ease that pain, won't ease that pain)<br>
+But I'ma purchase when that day is jerkin' (That day is jerkin', day is jerkin')<br>
+Pull off at Church's with Pirellis skrrtin' (Pirellis skrrtin', Pirellis skrrtin')<br>
+Gang signs out the window, ya bish (Ya bish, ya bish)<br>
+Hopin' all of 'em offend you, ya bish (Ya bish, ya bish)<br>
+They say your hood is a pot of gold (A pot of gold, a pot of gold)<br>
+And we gon' crash it when nobody's home<br>
+<br>
+It go Halle Berry or hallelujah<br>
+Pick your poison, tell me what you doin'<br>
+Everybody gon' respect the shooter<br>
+But the one in front of the gun lives forever<br>
+(The one in front of the gun, forever)<br>
+And I been hustlin' all day, this-a-way, that-a-way<br>
+Through canals and alleyways, just to say<br>
+Money trees is the perfect place for shade<br>
+And that's just how I feel, nah, nah<br>
+A dollar might just fuck your main bitch<br>
+That's just how I feel, nah<br>
+A dollar might say fuck them niggas that you came with<br>
+That's just how I feel, nah, nah<br>
+A dollar might just make that lane switch<br>
+That's just how I feel, nah<br>
+A dollar might turn to a million and we all rich<br>
+That's just how I feel<br>
+<br>
+Be the last one out to get this dough? No way<br>
+Love one of you bucket-headed hoes? No way<br>
+Hit the streets, then we break the code? No way<br>
+Hit the brakes when they on patrol? No way<br>
+Be the last one out to get this dough? No way<br>
+Love one of you bucket-headed hoes? No way<br>
+Hit the streets, then we break the code? No way<br>
+Hit the brakes when they on patrol? No way<br>
+<br>
+'Magine Rock up in them projects where them niggas pick your pockets<br>
+Santa Claus don't miss them stockings, liquors spillin', pistols poppin'<br>
+Bakin' soda, yola whippin', ain't no turkey on Thanksgivin'<br>
+My homeboy just domed a nigga, I just hope the Lord forgive him<br>
+Pots with cocaine residue, every day, I'm hustlin'<br>
+What else is a thug to do when you eatin' cheese from the government?<br>
+Gotta provide for my daughter 'nem, get the fuck up out my way, bish<br>
+Got that drum and I got them bands just like a parade, bish<br>
+Drop that work up in the bushes, hope them boys don't see my stash<br>
+If they do, tell the truth, this the last time you might see my ass<br>
+From the gardens where the grass ain't cut, them serpents lurkin', blood<br>
+Bitches sellin' pussy, niggas sellin' drugs, but it's all good<br>
+Broken promises, steal your watch and tell you what time it is<br>
+Take your J's and tell you to kick it where a Foot Locker is<br>
+In the streets with a heater under my dungarees<br>
+Dreams of me gettin' shaded under a money tree<br>
+<br>
+It go Halle Berry or hallelujah<br>
+Pick your poison, tell me what you doin'<br>
+Everybody gon' respect the shooter<br>
+But the one in front of the gun lives forever<br>
+(The one in front of the gun, forever)<br>
+And I been hustlin' all day, this-a-way, that-a-way<br>
+Through canals and alleyways, just to say<br>
+Money trees is the perfect place for shade<br>
+And that's just how I feel<br>
+<br>
+Kendrick, just bring my car back, man, I, I called in for another appointment<br>
+I figured you weren't gonna be back here on time anyway, look<br>
+Shit, shit, I just wanna get out the house, man<br>
+This man is on one, he feelin' good than a motherfucker<br>
+Shit, I'm tryna get my thing goin' too (Girl, I want your body)<br>
+I'm going over to Merlin house, just bring my car back (I want your body)<br>
+Shit, he faded, he feelin' good, look, listen to him<br>
+'Cause you got a big ol' fat ass<br>
+Girl, girl, I want your body<br>
+I want your body, 'cause of that big ol' fat ass, girl<br>
+See, he high as hell (I want your body, 'cause of that big ol'—)<br>
+Shit, he ain't even trippin' off them dominoes no more, just bring the car back<br>
+Did somebody say dominoes?<br>`
+},
+    {title: "Redbone", artist:"Childish Gambino", youtube: "https://www.youtube.com/embed/k49I5m1J6Is?si=Fw66WLvrwr8M0P2K", lyricText: 
+`Daylight<br>
+I wake up feelin' like you won't play right<br>
+I used to know, but now that shit don't feel right<br>
+It made me put away my pride<br>
+So long<br>
+You made a nigga wait for some, so long<br>
+You make it hard for boy like that to go on<br>
+I'm wishin' I could make this mine, oh<br>
+<br>
+(If you want it) Yeah<br>
+(You can have it) Oh, oh, oh<br>
+(If you need it) Ooh<br>
+(We can make it, oh)<br>
+If you want it<br>
+You can have it<br>
+<br>
+But stay woke<br>
+N***as creepin'<br>
+They gon' find you<br>
+Gon' catch you sleepin', ooh<br>
+Now stay woke<br>
+N***as creepin'<br>
+Now, don't you close your eyes<br>
+<br>
+Too late<br>
+You wanna make it right, but now it's too late<br>
+My peanut butter chocolate cake with Kool-Aid<br>
+I'm tryin' not to waste my time<br>
+<br>
+(If you want it) Oh<br>
+(You can have it) You can have it<br>
+(If you need it) You better believe in somethin'<br>
+(We can make it, oh)<br>
+If you want it<br>
+(You can have it) Ahh!<br>
+<br>
+But stay woke (Stay woke)<br>
+N***as creepin' (They be creepin')<br>
+They gon' find you (They gon' find you)<br>
+Gon' catch you sleepin', ooh (Gon' catch you sleepin', put your hands up now, baby)<br>
+Now stay woke<br>
+N***as creepin'<br>
+Now, don't you close your eyes<br>
+But stay woke (Ooh-ahh)<br>
+N***as creepin' (They gon' find you)<br>
+They gon' find you<br>
+Gon' catch you sleepin', ooh (Gon' catch you, gon' catch you)<br>
+Now stay woke<br>
+N***as creepin'<br>
+Now, don't you close your eyes<br>
+<br>
+How'd it get so scandalous?<br>
+Oh, how'd it get so scandalous?<br>
+Oh, oh, baby, you, how'd it get<br>
+How'd it get so scandalous?<br>
+How'd it get so scandalous?<br>
+But stay woke<br>
+But stay woke<br>`},
+    {title: "Shut up My Moms Calling", artist: "Hotel Ugly", youtube: "https://www.youtube.com/embed/a24EUd0zeqI?si=klQtgNAwmHRsLPCc", lyricText: 
+
+`I just want to rewind, I haven't seen you in a long time<br>
+You got me feeling so lonely<br>
+Even when you come through, I can tell<br>
+That it isn't you, so, baby, bring it in closely<br>
+Hate the way I love you, but you're so sweet<br>
+I always find a way to say the wrong things<br>
+I wish that we were laying in the same sheets<br>
+But lately you've been acting like you hardly know me<br>
+<br>
+
+Baby, come home (So, baby, won't you say some?), home<br>
+Baby, come home (So, baby, won't you say some?), home<br>
+Baby, come home (So, baby, won't you say some?), home<br>
+Baby, come home (So, baby, won't you say some?), home<br>
+<br>
+
+I just want to rewind, I haven't seen you in a long time<br>
+You got me feeling so lonely<br>
+Even when you come through, I can tell<br>
+That it isn't you, so, baby, bring it in closely<br>
+Hate the way I love you, but you're so sweet<br>
+I always find a way to say the wrong things<br>
+I wish that we were laying in the same sheets<br>
+But lately you've been acting like you hardly know me<br>
+
+<br>
+I've only recently began to fall<br>
+I feel the need to go and waste it all<br>
+I try to numb away the pain<br>
+I hope someone is watching me, watching me, watching me<br>
+
+<br>
+Baby, come home (So, baby, won't you say some?), home<br>
+Baby, come home (So, baby, won't you say some?), home<br>
+Baby, come home (So, baby, won't you say some?), home<br>
+Baby, come home (So, baby, won't you say some?), home<br>`}
+]
